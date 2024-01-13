d165344a41e8438f49065fe13313b2c249bc910d
GokaGokai <gokagokaigokai@gmail.com>
2024-01-13 05:29:02 -0500

    Update Projects UI

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,13 +15,13 @@ const Card: FC<CardProps> = ({ imgSrc, title, link, description, tools }) => {
     <a href={link} target="_blank" rel="noopener noreferrer" className="block">
       <div
         className="relative group flex items-center justify-center py-12 px-8 bg-accent
-      hover:bg-hoverPrimary flex-1 cursor-pointer min-w-fit sm:w-[600px] lg:w-[800px]
-        rounded-2xl overflow-hidden transition-all duration-700 ease-in-out"
+      hover:bg-hoverPrimary flex-1 cursor-pointer min-w-fit
+        rounded-2xl overflow-hidden transition-all duration-700 ease-in-out hover:scale-[1.01]"
       >
         <img
           src={imgSrc}
           alt=""
-          className="transition-all group-hover:pt-12 duration-700 backdrop-blur-0 group-hover:scale-105 ease-in-out md:max-h-[430px] max-h-[350px]"
+          className="transition-all rounded-lg group-hover:mt-12 duration-700 backdrop-blur-0 group-hover:scale-[1.02] ease-in-out md:max-h-[430px] md:w-auto max-h-[350px]"
         />
 
         <div className="text-textPrimary absolute top-0 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-all duration-700 ease-in-out text-center w-full px-8 z-10">
--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
 import cafe from "./assets/cafe.png";
 import raytracer from "./assets/color_plink2.ray1440p10spp.png";
-import ddr from "./assets/ddr.png";
+import ddr from "./assets/blitzpad.png";
 
 interface Project {
   id: number;
@@ -28,16 +28,16 @@ export const projects: Project[] = [
     img: raytracer,
     title: "Raytracer",
     link: "https://cdn.discordapp.com/attachments/900402165179359234/1035923195280236574/color_plink2.ray1440p10spp.png?ex=65abd8a8&is=659963a8&hm=341a59cb04de477e6e94f47a711fbf2f3808ece6637b597f7f15a9ffa7381188&",
-    description: "Raytracer made from scratch.",
+    description: "Ray tracing engine made from scratch.",
     tools: "C++, CMake",
   },
   {
     id: 3,
     category: "arduino",
     img: ddr,
-    title: "DDR Dance Pad",
+    title: "BlitzPad",
     link: "https://youtube.com/shorts/OpIH9xendsc",
-    description: "A dance pad made from scratch.",
+    description: "A DDR Dance Pad made from scratch.",
     tools: "Arduino, Circuitry, Woodworking, Soldering, Painting",
   },
 ];
